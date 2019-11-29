8540c82da42659b01c3d13574983ee0946e87eb4
duncup <dlmsoft.wolf@gmail.com>
2019-11-29 11:45:41 +0800

    test: fix

--- test.js
+++ test.js
@@ -1,14 +1,66 @@
 import test from 'ava';
 import alfyTest from 'alfy-test';
 
-test(async t => {
+test('Base test', async t => {
 	const alfy = alfyTest();
-	const result = await alfy('Rainbow');
-	console.log(t, result);
-	// T.deepEqual(result, [
-	// 	{
-	// 		title: 'Unicorn',
-	// 		subtitle: 'Rainbow'
-	// 	}
-	// ]);
+	const result = await alfy('test');
+	t.deepEqual(result, [
+		{
+			title: 'Google',
+			subtitle: 'https://zh.lmgtfy.me/?q=test&pp=1',
+			arg: 'https://zh.lmgtfy.me/?q=test&pp=1',
+			quicklookurl: 'https://zh.lmgtfy.me/?q=test&pp=1',
+			icon: {type: 'png', path: 'google.png'}
+		},
+		{
+			title: 'lmgtfy',
+			subtitle: 'https://zh.lmgtfy.me/?q=test&pp=1&s=l',
+			arg: 'https://zh.lmgtfy.me/?q=test&pp=1&s=l',
+			quicklookurl: 'https://zh.lmgtfy.me/?q=test&pp=1&s=l',
+			icon: {type: 'png', path: 'lmgtfy.png'}
+		},
+		{
+			title: 'Yahoo',
+			subtitle: 'https://zh.lmgtfy.me/?q=test&pp=1&s=y',
+			arg: 'https://zh.lmgtfy.me/?q=test&pp=1&s=y',
+			quicklookurl: 'https://zh.lmgtfy.me/?q=test&pp=1&s=y',
+			icon: {type: 'png', path: 'yahoo.png'}
+		},
+		{
+			title: 'Bing',
+			subtitle: 'https://zh.lmgtfy.me/?q=test&pp=1&s=b',
+			arg: 'https://zh.lmgtfy.me/?q=test&pp=1&s=b',
+			quicklookurl: 'https://zh.lmgtfy.me/?q=test&pp=1&s=b',
+			icon: {type: 'png', path: 'bing.png'}
+		},
+		{
+			title: 'Startpage',
+			subtitle: 'https://zh.lmgtfy.me/?q=test&pp=1&s=t',
+			arg: 'https://zh.lmgtfy.me/?q=test&pp=1&s=t',
+			quicklookurl: 'https://zh.lmgtfy.me/?q=test&pp=1&s=t',
+			icon: {type: 'png', path: 'startpage.png'}
+		},
+		{
+			title: 'Ask',
+			subtitle: 'https://zh.lmgtfy.me/?q=test&pp=1&&s=k',
+			arg: 'https://zh.lmgtfy.me/?q=test&pp=1&&s=k',
+			quicklookurl: 'https://zh.lmgtfy.me/?q=test&pp=1&&s=k',
+			icon: {type: 'png', path: 'ask.png'}
+		},
+		{
+			title: 'Aol',
+			subtitle: 'https://zh.lmgtfy.me/?q=test&pp=1&s=a',
+			arg: 'https://zh.lmgtfy.me/?q=test&pp=1&s=a',
+			quicklookurl: 'https://zh.lmgtfy.me/?q=test&pp=1&s=a',
+			icon: {type: 'png', path: 'aol.png'}
+		},
+		{
+			title: 'Duckduckgo',
+			subtitle: 'https://zh.lmgtfy.me/?q=test&pp=1&s=d',
+			arg: 'https://zh.lmgtfy.me/?q=test&pp=1&s=d',
+			quicklookurl: 'https://zh.lmgtfy.me/?q=test&pp=1&s=d',
+			icon: {type: 'png', path: 'duckduckgo.png'}
+		}
+	]
+	);
 });
